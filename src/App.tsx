e25152e5835cdc30eed914f486fea356e4dd86fe
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-07-16 22:33:17 +0400

    Learning types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 function App() {
+  const add = (n1: number, n2: number, showResult: boolean) => {
+    if (showResult) return n1 + n2;
+  };
+
+  const number1 = 5;
+  const number2 = 7.2;
+  const printResult = true;
+
+  const result = add(number1, number2, printResult);
+  console.log(result);
+
   return (
     <div className='App'>
       <h1>Understanding TypeScript</h1>
