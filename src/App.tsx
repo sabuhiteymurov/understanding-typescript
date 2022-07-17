d7ab9a884655ee6dbec168f3101a2a2e54b31555
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-07-17 16:17:23 +0400

    Function return types void & undefined

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,46 @@ import Basics from './Basics';
 function App() {
   Basics();
 
-  const combine = (input1: number | string, input2: number | string) => {
+  /*
+  type Combinable = number | string;
+  type ConversionDescriptor = 'as-number' | 'as-text';
+
+  const combine = (
+    input1: Combinable,
+    input2: Combinable,
+    resultType: ConversionDescriptor
+  ) => {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-      result = input1 + input2;
+    if (
+      (typeof input1 === 'number' && typeof input2 === 'number') ||
+      resultType === 'as-number'
+    ) {
+      result = +input1 + +input2;
     } else {
       result = input1.toString() + input2.toString();
     }
     return result;
   };
 
-  const combinedAges = combine(30, 20);
+  const combinedAges = combine(30, 20, 'as-number');
   console.log(combinedAges);
 
-  const combinedNames = combine('Sabuhi', 'Teymurov');
+  const combinedStringAges = combine('20', '39', 'as-number');
+  console.log(combinedStringAges);
+
+  const combinedNames = combine('Sabuhi', 'Teymurov', 'as-text');
   console.log(combinedNames);
+  */
+
+  const add = (n1: number, n2: number) => {
+    return n1 + n2;
+  };
+
+  const printResult = (num: number): void => {
+    console.log('Result: ' + num);
+  };
+
+  printResult(add(5, 12));
 
   return (
     <div className='App'>
