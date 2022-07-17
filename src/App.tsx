65a4bfda1661ff28f91472250ec40febbf8b2962
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-07-17 20:12:28 +0400

    Added CodeWars challenge solutions for TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Basics from './Basics';
+import CodeWars from './CodeWars';
 
 function App() {
   Basics();
+  CodeWars();
 
   return (
     <div className='App'>
