ce9925b44a717764488dd29473ace7efd4c2c2f6
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-07-17 14:08:50 +0400

    Union Types

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ import Basics from './Basics';
 function App() {
   Basics();
 
+  const combine = (input1: number | string, input2: number | string) => {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+      result = input1 + input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+    return result;
+  };
+
+  const combinedAges = combine(30, 20);
+  console.log(combinedAges);
+
+  const combinedNames = combine('Sabuhi', 'Teymurov');
+  console.log(combinedNames);
+
   return (
     <div className='App'>
       <h1>Understanding TypeScript</h1>
--- src/Basics.ts
+++ src/Basics.ts
@@ -1,16 +1,35 @@
 const Basics = () => {
+  // const Person: {
+  //   name: string;
+  //   age: number;
+  //   hobbies: string[];
+  //   role: [number, string];
+  // } = {
+  //   name: 'Sabuhi',
+  //   age: 30,
+  //   hobbies: ['Sports', 'Cooking'],
+  //   role: [2, 'author'], // tuple
+  // };
+
+  // Person.role.push('admin'); // Doesn't give an error on tuples
+  // Person.role[1] = 10;
+
+  // const ADMIN = 0;
+  // const READ_ONLY = 1;
+  // const AUTHOR = 2;
+
+  enum Role {
+    ADMIN = 1,
+    READ_ONLY,
+    AUTHOR,
+  }
+
   const Person = {
     name: 'Sabuhi',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN,
   };
-
-  let schoolItems: string[];
-  schoolItems = ['Pencil', 'A4'];
-
-  Person.hobbies.forEach((hobby) => console.log(hobby));
-
-  console.log('Learning the basics');
 };
 
 export default Basics;
