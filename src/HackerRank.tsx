c2435f8f41203caf98e362c19dea5141e6c19074
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-07-20 13:18:01 +0400

    HackerRank TypeScript #6

--- src/HackerRank.tsx
+++ src/HackerRank.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HackerRank = () => {
   /*
-  function compareTriplets(a: number[], b: number[]) {
+  function compareTriplets(a: number[], b: number[]):number[] {
     // Write your code here
     const scores = [0, 0];
     for (let i = 0; i < a.length; i++) {
@@ -13,7 +13,7 @@ const HackerRank = () => {
   }
   */
   /*
-  function aVeryBigSum(ar: number[]) {
+  function aVeryBigSum(ar: number[]):number {
     // Write your code here
     const initialValue = 0;
     return (
@@ -25,7 +25,7 @@ const HackerRank = () => {
   }
   */
   /*
-  function diagonalDifference(arr: number[][]) {
+  function diagonalDifference(arr: number[][]):number {
     // Write your code here
     let leftSide = 0;
     let rightSide = 0;
@@ -36,6 +36,37 @@ const HackerRank = () => {
       rightSide += arr[i][arr[0].length - (i + 1)];
     }
     return Math.abs(leftSide - rightSide);
+  }
+  */
+  /*
+  function plusMinus(arr: number[]): void {
+    // Write your code here
+    let numberOfP = 0;
+    let numberOfN = 0;
+    let numberOfZ = 0;
+    arr.forEach((n) => {
+      if (n > 0) {
+        numberOfP++;
+      } else if (n === 0) {
+        numberOfZ++;
+      } else {
+        numberOfN++;
+      }
+    });
+    console.log((numberOfP / arr.length).toFixed(6));
+    console.log((numberOfN / arr.length).toFixed(6));
+    console.log((numberOfZ / arr.length).toFixed(6));
+  }
+  */
+  /*
+  function staircase(n: number): void {
+    // Write your code here
+    let whiteSpace = ' ';
+    for (let i = n - 1, a = 1; i > -1 && a < n + 1; i--, a++) {
+      if (i === 0) whiteSpace = '';
+      console.log(whiteSpace.repeat(i) + '#'.repeat(a));
+    }
+    console.log('' + '#'.repeat(6));
   }
   */
 
