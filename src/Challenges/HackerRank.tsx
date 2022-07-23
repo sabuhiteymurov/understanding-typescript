ea16628f78b3f0221b3444d5459c38b492c99868
Sabuhi Teymurov <teymurov.sabuhi@gmail.com>
2022-07-23 14:45:39 +0400

    HackerRank #16

--- src/Challenges/HackerRank.tsx
+++ src/Challenges/HackerRank.tsx
@@ -189,6 +189,36 @@ const HackerRank = () => {
     return max;
   };
   */
+  /*
+  function climbingLeaderboard(ranked: number[], player: number[]): number[] {
+    // Write your code here
+    // Solution #1
+    
+    let rank = [];
+    for (let i = 0; i < ranked.length; i++) {
+      if (rank.length === 0) {
+        rank.push(ranked[i]);
+      } else {
+        if (rank[rank.length - 1] === ranked[i]) {
+          continue;
+        }
+        rank.push(ranked[i]);
+      }
+    }
+
+    let res = [];
+    let lowestRank = rank.length;
+    for (let i = 0; i < player.length; i++) {
+      while (player[i] >= rank[lowestRank - 1] && lowestRank > 0) {
+        rank.pop();
+        lowestRank--;
+      }
+      res.push(lowestRank + 1);
+    }
+    
+    return res;
+  }
+  */
 
   return <div></div>;
 };
